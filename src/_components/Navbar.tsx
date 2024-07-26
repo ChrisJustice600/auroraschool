732ac777f99380ea405c6d89a41e171657228393
unknown <christianmabeladi@gmail.com>
2024-07-26 13:26:00 +0100

    fix 2 : navbar

--- src/_components/Navbar.tsx
+++ src/_components/Navbar.tsx
@@ -10,13 +10,13 @@ export default function Navbar() {
   return (
     <div>
       {" "}
-      <nav className="bg-gradient-to-r from-purple-600 to-indigo-700 shadow-lg">
+      <nav className="bg-gradient-to-r from-purple-100 to-indigo-100 shadow-lg">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex justify-between h-16">
             <div className="flex items-center">
               <a href="/" className="flex-shrink-0">
                 <svg
-                  className="h-8 w-8 text-white"
+                  className="h-8 w-8 text-primary"
                   fill="none"
                   viewBox="0 0 24 24"
                   stroke="currentColor"
@@ -29,38 +29,38 @@ export default function Navbar() {
                   />
                 </svg>
               </a>
-              <div className="ml-4 text-xl font-bold text-white">
+              <div className="ml-4 text-xl font-bold text-foreground">
                 École Innovante
               </div>
             </div>
-            <div className="hidden md:flex items-center space-x-4">
+            <div className="hidden md:flex items-center space-x-4 font-bold">
               <a
                 href="/"
-                className="nav-item text-white hover:bg-purple-500 px-3 py-2 rounded-md text-sm font-medium transition-all duration-300 hover:scale-105"
+                className="nav-item text-foreground hover:text-popover hover:bg-secondary-foreground px-3 py-2 rounded-md text-sm font-medium transition-all duration-300 hover:scale-105"
               >
                 Accueil
               </a>
               <a
                 href="/programmes"
-                className="nav-item text-white hover:bg-purple-500 px-3 py-2 rounded-md text-sm font-medium transition-all duration-300 hover:scale-105"
+                className="nav-item text-foreground hover:text-popover hover:bg-secondary-foreground px-3 py-2 rounded-md text-sm font-medium transition-all duration-300 hover:scale-105"
               >
                 Programmes
               </a>
               <a
                 href="/admissions"
-                className="nav-item text-white hover:bg-purple-500 px-3 py-2 rounded-md text-sm font-medium transition-all duration-300 hover:scale-105"
+                className="nav-item text-foreground hover:text-popover hover:bg-secondary-foreground px-3 py-2 rounded-md text-sm font-medium transition-all duration-300 hover:scale-105"
               >
                 Admissions
               </a>
               <a
                 href="/vie-etudiante"
-                className="nav-item text-white hover:bg-purple-500 px-3 py-2 rounded-md text-sm font-medium transition-all duration-300 hover:scale-105"
+                className="nav-item text-foreground hover:text-popover hover:bg-secondary-foreground px-3 py-2 rounded-md text-sm font-medium transition-all duration-300 hover:scale-105"
               >
                 Vie Étudiante
               </a>
               <a
                 href="/contact"
-                className="nav-item text-white hover:bg-purple-500 px-3 py-2 rounded-md text-sm font-medium transition-all duration-300 hover:scale-105"
+                className="nav-item text-foreground hover:text-popover hover:bg-secondary-foreground px-3 py-2 rounded-md text-sm font-medium transition-all duration-300 hover:scale-105"
               >
                 Contact
               </a>
@@ -68,16 +68,16 @@ export default function Navbar() {
             <div className="md:hidden flex items-center">
               <button
                 id="hamburger"
-                className={`hamburger text-white focus:outline-none ${
+                className={`hamburger text-foreground focus:outline-none ${
                   isOpen ? "active" : ""
                 }`}
                 aria-label="Menu"
                 onClick={toggleMenu}
               >
-                <div className="w-6 h-6 flex flex-col justify-between">
-                  <span className="hamburger-line h-1 w-6 bg-white rounded-full line-1"></span>
-                  <span className="hamburger-line h-1 w-6 bg-white rounded-full line-2"></span>
-                  <span className="hamburger-line h-1 w-6 bg-white rounded-full line-3"></span>
+                <div className="w-6 h-6 flex flex-col justify-between text-primary">
+                  <span className="hamburger-line h-1 w-6 bg-primary rounded-full line-1"></span>
+                  <span className="hamburger-line h-1 w-6 bg-primary rounded-full line-2"></span>
+                  <span className="hamburger-line h-1 w-6 bg-primary rounded-full line-3"></span>
                 </div>
               </button>
             </div>
@@ -85,34 +85,34 @@ export default function Navbar() {
         </div>
 
         <div id="mobile-menu" className={`md:hidden ${isOpen ? "" : "hidden"}`}>
-          <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3 mobile-menu bg-purple-700">
+          <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3 mobile-menu bg-primary-foreground">
             <a
               href="/"
-              className="text-white hover:bg-purple-500 block px-3 py-2 rounded-md text-base font-medium"
+              className="text-foreground hover:bg-secondary-foreground block px-3 py-2 rounded-md text-base font-medium"
             >
               Accueil
             </a>
             <a
               href="/programmes"
-              className="text-white hover:bg-purple-500 block px-3 py-2 rounded-md text-base font-medium"
+              className="text-foreground hover:bg-secondary-foreground block px-3 py-2 rounded-md text-base font-medium"
             >
               Programmes
             </a>
             <a
               href="/admissions"
-              className="text-white hover:bg-purple-500 block px-3 py-2 rounded-md text-base font-medium"
+              className="text-foreground hover:bg-secondary-foreground block px-3 py-2 rounded-md text-base font-medium"
             >
               Admissions
             </a>
             <a
               href="/vie-etudiante"
-              className="text-white hover:bg-purple-500 block px-3 py-2 rounded-md text-base font-medium"
+              className="text-foreground hover:bg-secondary-foreground block px-3 py-2 rounded-md text-base font-medium"
             >
               Vie Étudiante
             </a>
             <a
               href="/contact"
-              className="text-white hover:bg-purple-500 block px-3 py-2 rounded-md text-base font-medium"
+              className="text-foreground hover:bg-secondary-foreground block px-3 py-2 rounded-md text-base font-medium"
             >
               Contact
             </a>
