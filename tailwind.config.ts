13ec9e01dfc697a087fc1b9ccf12407468cb5ef5
unknown <christianmabeladi@gmail.com>
2024-07-27 23:17:08 +0100

    fix: change colors to custom scrollbar

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -74,7 +74,7 @@ const config = {
         },
       },
       animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-down": "accordion-down 0.2s ease",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
