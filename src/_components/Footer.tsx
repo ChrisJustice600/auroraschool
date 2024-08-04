33421b9fe8c1ce10f358ea7df4abd83fd9d9273b
unknown <christianmabeladi@gmail.com>
2024-08-04 23:50:54 +0100

    fix footer section

--- src/_components/Footer.tsx
+++ src/_components/Footer.tsx
@@ -8,7 +8,7 @@ export default function Footer() {
     <Section className="">
       <footer className=" text-muted-foreground font-roboto">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
             <div className="space-y-4">
               <div className="flex items-center">
                 <a href="/" className="flex-shrink-0">
@@ -20,30 +20,17 @@ export default function Footer() {
                   />
                 </a>
               </div>{" "}
-              <h3 className="text-2xl font-medium text-left text-primary">
+              <h4 className="text-xl font-medium !text-left text-primary">
                 Ecole chretienne aurora dawn
-              </h3>
-              <p className="text-sm">
+              </h4>
+              <p className="text-sm text-left">
                 Empowering innovation through cutting-edge solutions. We're here
                 to make a difference.
               </p>
               <div className="flex space-x-4">
                 <a
-                  href="https://twitter.com"
-                  className="text-gray-400 hover:text-white transition-colors duration-300"
-                >
-                  <svg
-                    className="w-6 h-6"
-                    fill="currentColor"
-                    viewBox="0 0 24 24"
-                    aria-hidden="true"
-                  >
-                    <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
-                  </svg>
-                </a>
-                <a
                   href="https://facebook.com"
-                  className="text-gray-400 hover:text-white transition-colors duration-300"
+                  className="text-gray-400 hover:text-gray-800 transition-colors duration-300"
                 >
                   <svg
                     className="w-6 h-6"
@@ -60,30 +47,49 @@ export default function Footer() {
                 </a>
                 <a
                   href="https://instagram.com"
-                  className="text-gray-400 hover:text-white transition-colors duration-300"
+                  className="text-gray-400 hover:text-gray-800 transition-colors duration-300"
                 >
                   <svg
                     className="w-6 h-6"
                     fill="currentColor"
-                    viewBox="0 0 24 24"
-                    aria-hidden="true"
+                    viewBox="0 0 308 308"
                   >
-                    <path
-                      fillRule="evenodd"
-                      d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z"
-                      clipRule="evenodd"
-                    />
+                    <g id="XMLID_468_">
+                      <path
+                        id="XMLID_469_"
+                        d="M227.904,176.981c-0.6-0.288-23.054-11.345-27.044-12.781c-1.629-0.585-3.374-1.156-5.23-1.156
+		c-3.032,0-5.579,1.511-7.563,4.479c-2.243,3.334-9.033,11.271-11.131,13.642c-0.274,0.313-0.648,0.687-0.872,0.687
+		c-0.201,0-3.676-1.431-4.728-1.888c-24.087-10.463-42.37-35.624-44.877-39.867c-0.358-0.61-0.373-0.887-0.376-0.887
+		c0.088-0.323,0.898-1.135,1.316-1.554c1.223-1.21,2.548-2.805,3.83-4.348c0.607-0.731,1.215-1.463,1.812-2.153
+		c1.86-2.164,2.688-3.844,3.648-5.79l0.503-1.011c2.344-4.657,0.342-8.587-0.305-9.856c-0.531-1.062-10.012-23.944-11.02-26.348
+		c-2.424-5.801-5.627-8.502-10.078-8.502c-0.413,0,0,0-1.732,0.073c-2.109,0.089-13.594,1.601-18.672,4.802
+		c-5.385,3.395-14.495,14.217-14.495,33.249c0,17.129,10.87,33.302,15.537,39.453c0.116,0.155,0.329,0.47,0.638,0.922
+		c17.873,26.102,40.154,45.446,62.741,54.469c21.745,8.686,32.042,9.69,37.896,9.69c0.001,0,0.001,0,0.001,0
+		c2.46,0,4.429-0.193,6.166-0.364l1.102-0.105c7.512-0.666,24.02-9.22,27.775-19.655c2.958-8.219,3.738-17.199,1.77-20.458
+		C233.168,179.508,230.845,178.393,227.904,176.981z"
+                      />
+                      <path
+                        id="XMLID_470_"
+                        d="M156.734,0C73.318,0,5.454,67.354,5.454,150.143c0,26.777,7.166,52.988,20.741,75.928L0.212,302.716
+		c-0.484,1.429-0.124,3.009,0.933,4.085C1.908,307.58,2.943,308,4,308c0.405,0,0.813-0.061,1.211-0.188l79.92-25.396
+		c21.87,11.685,46.588,17.853,71.604,17.853C240.143,300.27,308,232.923,308,150.143C308,67.354,240.143,0,156.734,0z
+		 M156.734,268.994c-23.539,0-46.338-6.797-65.936-19.657c-0.659-0.433-1.424-0.655-2.194-0.655c-0.407,0-0.815,0.062-1.212,0.188
+		l-40.035,12.726l12.924-38.129c0.418-1.234,0.209-2.595-0.561-3.647c-14.924-20.392-22.813-44.485-22.813-69.677
+		c0-65.543,53.754-118.867,119.826-118.867c66.064,0,119.812,53.324,119.812,118.867
+		C276.546,215.678,222.799,268.994,156.734,268.994z"
+                      />
+                    </g>
                   </svg>
                 </a>
               </div>
             </div>
-            <div>
+            <div className="text-left">
               <h4 className="text-lg font-semibold text-primary mb-4">Menu</h4>
-              <ul className="space-y-2">
+              <ul className="space-y-2 font-bold">
                 <li>
                   <a
                     href="https://websim.io/about"
-                    className="hover:text-white transition-colors duration-300"
+                    className="hover:text-gray-800 transition-colors duration-300"
                   >
                     A propos
                   </a>
@@ -91,7 +97,7 @@ export default function Footer() {
                 <li>
                   <a
                     href="https://websim.io/services"
-                    className="hover:text-white transition-colors duration-300"
+                    className="hover:text-gray-800 transition-colors duration-300"
                   >
                     Services
                   </a>
@@ -99,7 +105,7 @@ export default function Footer() {
                 <li>
                   <a
                     href="https://websim.io/portfolio"
-                    className="hover:text-white transition-colors duration-300"
+                    className="hover:text-gray-800 transition-colors duration-300"
                   >
                     Portfolio
                   </a>
@@ -107,14 +113,14 @@ export default function Footer() {
                 <li>
                   <a
                     href="https://websim.io/contact"
-                    className="hover:text-white transition-colors duration-300"
+                    className="hover:text-gray-800 transition-colors duration-300"
                   >
                     Contact
                   </a>
                 </li>
               </ul>
             </div>
-            <div>
+            <div className="text-left">
               <h4 className="text-lg font-semibold text-primary mb-4">
                 Resources
               </h4>
@@ -122,7 +128,7 @@ export default function Footer() {
                 <li>
                   <a
                     href="https://websim.io/faq"
-                    className="hover:text-white transition-colors duration-300"
+                    className="hover:text-gray-800 transition-colors duration-300"
                   >
                     FAQ
                   </a>
@@ -130,10 +136,10 @@ export default function Footer() {
               </ul>
             </div>
           </div>
-          <div className="border-t border-primary mt-8 pt-8 flex flex-col sm:flex-row justify-between items-center">
-            <p className="text-sm text-left">
+          <div className="border-t border-primary mt-8 pt-8 flex flex-col sm:flex-row  ">
+            <span className="text-sm text-left">
               &copy; 2024 Ecole chretienne aurora dawn. Tout droit reservé.
-            </p>
+            </span>
           </div>
         </div>
       </footer>
